5197c34247c494c6b6b585e5e5ee3a521b2875e3
Mario Jose Castro Taks <vertigo2040@gmail.com>
2025-09-24 20:55:53 -0600

    Se crean funciones para trabajar en BD de SQLITE

--- Web/gastoweb.py
+++ Web/gastoweb.py
@@ -8,10 +8,10 @@ from datetime import datetime, timedelta
 # Nombre del archivo de la base de datos
 ARCHIVO_BD = 'gastos.db'
 
-# Inicializar la base de datos
+# Inicializar la base de datos y crear la tabla si no existe, si existe no hace nada
 def inicializar_base_de_datos():
-    conexion = sqlite3.connect(ARCHIVO_BD)
-    cursor = conexion.cursor()
+    conexion = sqlite3.connect(ARCHIVO_BD) # Conectar a la base de datos (o crearla si no existe)
+    cursor = conexion.cursor() # Crear un cursor para ejecutar comandos SQL
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS gastos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,6 +21,24 @@ def inicializar_base_de_datos():
             fecha TEXT NOT NULL
         )
     ''')
+    conexion.commit() # Guardar los cambios
+    conexion.close() # Cerrar la conexión
+#%%
+# Guardar un nuevo gasto
+def guardar_gasto(producto, categoria, monto, fecha):
+    conexion = sqlite3.connect(ARCHIVO_BD)
+    cursor = conexion.cursor()
+    cursor.execute('''
+        INSERT INTO gastos (producto, categoria, monto, fecha)
+        VALUES (?, ?, ?, ?)
+    ''', (producto, categoria, monto, fecha))
     conexion.commit()
     conexion.close()
+#%%
+# Cargar todos los gastos (sin filtros) para mostrar el pie general.
+def cargar_todos_los_gastos():
+    conexion = sqlite3.connect(ARCHIVO_BD)
+    df = pd.read_sql_query("SELECT * FROM gastos", conexion)
+    conexion.close()
+    return df
 #%%
